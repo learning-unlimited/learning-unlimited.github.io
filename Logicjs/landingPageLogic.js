62436dc861dec92f7c2e6d541b88a3869509dec2
SuperexMack <unilexerox@gmail.com>
2026-03-09 01:19:59 +0530

    Revamped the landing page and fixed the hamburger feature bug

--- Logicjs/landingPageLogic.js
+++ Logicjs/landingPageLogic.js
@@ -1,26 +1,29 @@
-function loadComponent(path, id) {
+function loadComponent(path, id, callback) {
   fetch(path)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
+      if (callback) callback();
     });
 }
 
-loadComponent("./Components/Navbar.html", "navbar");
+loadComponent("./Components/Navbar.html", "navbar", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
+
 loadComponent("./Components/HeroSection.html", "hero-section");
 loadComponent("./Components/About.html", "about-section");
 loadComponent("./Components/Luworking.html", "working-lu");
 loadComponent("./Components/Lustartchapter.html", "start-chapter");
 loadComponent("./Components/Footer.html", "footer");
 
-
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
-
 let insertDate = document.getElementById("getfullyear");
 
-insertDate.textContent = new Date().getFullYear();
+if (insertDate) {
+  insertDate.textContent = new Date().getFullYear();
+}
