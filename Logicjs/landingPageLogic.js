71b5c722d030bd02b7615f41f17e48b32367c78d
SuperexMack <unilexerox@gmail.com>
2026-03-09 22:29:45 +0530

    Revamped the landing page

--- Logicjs/landingPageLogic.js
+++ Logicjs/landingPageLogic.js
@@ -1,31 +1,12 @@
-function loadComponent(path, id, callback) {
-  fetch(path)
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById(id).innerHTML = data;
-      if (callback) callback();
-    });
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+let insertDate = document.getElementById("getfullyear");
+
+if (insertDate) {
+  insertDate.textContent = new Date().getFullYear();
 }
 
-loadComponent("./Components/Navbar.html", "navbar", () => {
-  const hamburger = document.getElementById("hamburger");
-  const navLinks = document.getElementById("nav-links");
-
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
-
-loadComponent("./Components/HeroSection.html", "hero-section");
-loadComponent("./Components/About.html", "about-section");
-loadComponent("./Components/Luworking.html", "working-lu");
-loadComponent("./Components/Lustartchapter.html", "start-chapter");
-
-loadComponent("./Components/Footer.html", "footer" , ()=>{
-  
-  let insertDate = document.getElementById("getfullyear");
-
-  if (insertDate) {
-    insertDate.textContent = new Date().getFullYear();
-  }
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
